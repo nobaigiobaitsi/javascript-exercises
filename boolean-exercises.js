1a3555e2d6764928b2af081afa5652f080c0111d
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-07-10 18:53:27 +0300

    Saw boolean exer.

--- boolean-exercises.js
+++ boolean-exercises.js
@@ -1,6 +1,9 @@
 // If value is falsy(0, 0.0, undefined, null, false, " ") -> (\ud83E\uDD25)emoji + Falsy
-// If value is truthy -> (u2705)green tick + Truthy
+// If value is truthy -> (\u2705)green tick + Truthy
 
-const checkTruthy = value => {
+const checkTruthy = value => value ? "\u2705 Truthy" : "\uD83E\uDD25 Falsy"
 
-}
+console.log(checkTruthy([]))
+console.log(checkTruthy(0))
+console.log(checkTruthy('false'))
+
